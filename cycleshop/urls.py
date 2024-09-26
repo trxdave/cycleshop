1e12c53d496ecb021a8117b9ca1b6422bdf6fb5d
David Cotter <daveguy28@gmail.com>
2024-09-26 11:42:17 +0000

    Fix the base and css

--- cycleshop/urls.py
+++ cycleshop/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -24,4 +26,6 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('products/', include('products.urls')),
     path('accounts/', include('allauth.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
