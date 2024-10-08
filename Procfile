a5748b14728700ac25734370c37c822b7459430e
David Cotter <daveguy28@gmail.com>
2024-10-08 14:50:29 +0000

    intall gunicorn, and requirements.txt

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn cycleshop.wsgi:application
--- cycleshop/settings.py
+++ cycleshop/settings.py
@@ -99,16 +99,17 @@ WSGI_APPLICATION = 'cycleshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
