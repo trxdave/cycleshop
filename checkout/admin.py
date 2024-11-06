d195d9966659807f308d4500e90fb675a4374727
David Cotter <daveguy28@gmail.com>
2024-11-06 13:32:03 +0000

    Checkout Order in Django Admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Order
 
-# Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'full_name', 'email', 'phone_number', 'total', 'date')
+    search_fields = ('id', 'full_name', 'email')
+    list_filter = ('date',)
+    readonly_fields = ('date',)
+
+    fieldsets = (
+        ('Order Information', {
+            'fields': ('user', 'full_name', 'email', 'phone_number', 'total')
+        }),
+        ('Address Information', {
+            'fields': ('address', 'city', 'postal_code', 'country')
+        }),
+        ('Additional Info', {
+            'fields': ('date',)
+        }),
+    )
